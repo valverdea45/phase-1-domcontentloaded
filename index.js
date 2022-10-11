c41717e2040fd0d0ada6a1145195989a6e1c2213
Adrian <adrianvalverde45@gmail.com>
2022-10-10 22:12:22 -0400

    commit tree 007ce458e5078c218c4ee0f8bfeccd0840c10c35

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function (){
+    let theWords = document.getElementById("text")
+    theWords.textContent = `This is really cool!`
+})
